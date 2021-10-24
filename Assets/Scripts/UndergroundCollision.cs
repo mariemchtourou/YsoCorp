af0586f1de303d439fc047c541d5ad27ecd40889
mariemchtourou <49189974+mariemchtourou@users.noreply.github.com>
2021-10-24 14:31:16 +0200

    LevelBarProgressandObstacles

---
+++ Assets/Scripts/UndergroundCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndergroundCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        string tag = other.tag;
+        if (tag.Equals("Object"))
+        {
+            Debug.Log("object");
+        }
+        if (tag.Equals("Obstacle"))
+        {
+            Debug.Log("obstacle");
+        }
+    }
+}
